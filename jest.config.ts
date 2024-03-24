33a41389b20daeb792881b4473628b3c722170f7
Lauv <pussaaon@gmail.com>
2024-03-24 14:51:57 +0700

    add test coverage collection excluding app's parts.

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,19 @@
 import type { Config } from "@jest/types";
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const baseDir: string = "<rootDir>/src";
 const baseTestDir: string = "<rootDir>/tests";
 
 const config: Config.InitialOptions = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: [`${baseDir}/**/*.ts`],
+    coveragePathIgnorePatterns: [
+        `${baseDir}/app.ts`,
+        `${baseDir}/utils/helpers.ts`,
+    ],
     testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
