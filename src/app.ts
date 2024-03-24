55df24989ecf8b775bacce6ef5a2cee56ff79706
Lauv <pussaaon@gmail.com>
2024-03-24 13:12:16 +0700

    implement main app to simulate pub-subs events

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,143 @@
+import { appConfig } from "./appConfig";
+import { EventType } from "./events/EventType";
+import { StockLevelInsufficientEvent } from "./events/StockLevelInsufficientEvent";
+import { StockLevelLowEvent } from "./events/StockLevelLowEvent";
+import { StockLevelOkEvent } from "./events/StockLevelOkEvent";
+import { IMachineRepository } from "./models/IMachineRepository";
 import { Machine } from "./models/Machine";
+import { MachineRepository } from "./models/MachineRepository";
 import { IPublishSubscribeService } from "./pubsubs/IPublishSubscribeService";
+import { ISubscriber } from "./pubsubs/ISubscriber";
+import { MachineEventPublisher } from "./pubsubs/MachineEventPublisher";
+import { MachineRefillSubscriber } from "./pubsubs/MachineRefillSubscriber";
 import { MachineSaleSubscriber } from "./pubsubs/MachineSaleSubscriber";
-import { generateRandomEvent } from "./utils/helpers";
+import { StockWarningSubscriber } from "./pubsubs/StockWarningSubscriber";
+import { IMachineStockHandler } from "./services/IMachineStockHandler";
+import { MachineStockHandler } from "./services/MachineStockHandler";
+import { generateMachineIds, generateRandomEvent } from "./utils/helpers";
 import { EventEmitter } from "events";
+
+function setupMachineRepository(totalMachine: number): IMachineRepository {
+    const machines: Machine[] = generateMachineIds(totalMachine).map(
+        (id) => new Machine(id, appConfig.defaultStockLevel)
+    );
+    return new MachineRepository(machines);
+}
+
+function setupEventPublisher(
+    eventEmiiter: EventEmitter,
+    machineRepository: IMachineRepository
+): IPublishSubscribeService {
+    const machineStockHandler: IMachineStockHandler = new MachineStockHandler();
+    const machineEventPublisher: IPublishSubscribeService =
+        new MachineEventPublisher();
+
+    const saleSubscriber: ISubscriber = new MachineSaleSubscriber(
+        eventEmiiter,
+        machineRepository,
+        machineStockHandler
+    );
+    const refillSubcriber: ISubscriber = new MachineRefillSubscriber(
+        eventEmiiter,
+        machineRepository,
+        machineStockHandler
+    );
+    const stockWarningSubscriber: ISubscriber = new StockWarningSubscriber(
+        machineRepository,
+        machineStockHandler
+    );
+
+    machineEventPublisher.subscribe(EventType.SALE, saleSubscriber);
+    machineEventPublisher.subscribe(EventType.REFILL, refillSubcriber);
+    machineEventPublisher.subscribe(
+        EventType.STOCK_LEVEL_LOW,
+        stockWarningSubscriber
+    );
+    machineEventPublisher.subscribe(
+        EventType.STOCK_LEVEL_OK,
+        stockWarningSubscriber
+    );
+    machineEventPublisher.subscribe(
+        EventType.STOCK_LEVEL_INSUFFICIENT,
+        stockWarningSubscriber
+    );
+
+    return machineEventPublisher;
+}
+
+/* handle stock alerts event from sale and refill events */
+function setupChainedEventListeners(
+    eventEmitter: EventEmitter,
+    machineEventPublisher: IPublishSubscribeService
+) {
+    eventEmitter.addListener(
+        EventType.STOCK_LEVEL_LOW,
+        (event: StockLevelLowEvent) => {
+            machineEventPublisher.publish(event);
+        }
+    );
+
+    eventEmitter.addListener(
+        EventType.STOCK_LEVEL_OK,
+        (event: StockLevelOkEvent) => {
+            machineEventPublisher.publish(event);
+        }
+    );
+
+    eventEmitter.addListener(
+        EventType.STOCK_LEVEL_INSUFFICIENT,
+        (event: StockLevelInsufficientEvent) => {
+            machineEventPublisher.publish(event);
+        }
+    );
+}
 
 // program
 (async () => {
-    // create 3 machines with a quantity of 10 stock
-    const machines: Machine[] = [
-        new Machine("001"),
-        new Machine("002"),
-        new Machine("003"),
-    ];
+    let totalMachine = 10;
+    const testEventsPerMachine = 10;
+    const machineRepository: IMachineRepository =
+        setupMachineRepository(totalMachine);
 
-    // create a machine sale event subscriber. inject the machines (all subscribers should do this)
     const eventEmitter = new EventEmitter();
-    const saleSubscriber = new MachineSaleSubscriber(eventEmitter, machines);
 
-    // create the PubSub service
-    const pubSubService: IPublishSubscribeService =
-        null as unknown as IPublishSubscribeService; // implement and fix this
+    const machineEventPublisher = setupEventPublisher(
+        eventEmitter,
+        machineRepository
+    );
 
-    // create 5 random events
-    const events = [1, 2, 3, 4, 5].map((i) => generateRandomEvent());
+    setupChainedEventListeners(eventEmitter, machineEventPublisher);
 
-    // publish the events
-    events.map(pubSubService.publish);
+    Array.from({ length: totalMachine * testEventsPerMachine }, () => {
+        const event = generateRandomEvent(totalMachine);
+        machineEventPublisher.publish(event);
+    });
+
+    console.log("------- Add New Machine -------");
+    const newMachineCount = 3;
+    generateMachineIds(totalMachine + newMachineCount)
+        .slice(-newMachineCount)
+        .forEach((id) =>
+            machineRepository.addMachine(
+                new Machine(id, appConfig.defaultStockLevel)
+            )
+        );
+    totalMachine += newMachineCount;
+
+    // simulate random events including added machines
+    Array.from({ length: totalMachine * testEventsPerMachine }, () => {
+        const event = generateRandomEvent(totalMachine);
+        machineEventPublisher.publish(event);
+    });
+
+    console.log("------- Machines' Final Stock Level -------");
+    machineRepository
+        .getMachines()
+        .map((machines) =>
+            machines.forEach((machine) =>
+                console.log(
+                    ` current stock ${machine.id} is ${machine.stockLevel}`
+                )
+            )
+        );
 })();
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,22 +2,24 @@ import { IEvent } from "../events/IEvent";
 import { MachineRefillEvent } from "../events/MachineRefillEvent";
 import { MachineSaleEvent } from "../events/MachineSaleEvent";
 
-const randomMachine = (): string => {
-    const random = Math.random() * 3;
-    if (random < 1) {
-        return "001";
-    } else if (random < 2) {
-        return "002";
-    }
-    return "003";
+const randomMachine = (total_machine: number): string => {
+    const random = Math.random() * total_machine;
+    const id = Math.ceil(random).toFixed(0);
+    return id.toString().padStart(3, "0");
 };
 
-export const generateRandomEvent = (): IEvent => {
+export const generateMachineIds = (total_machine: number): string[] => {
+    return Array.from({ length: total_machine }, (_, index) =>
+        (index + 1).toString().padStart(3, "0")
+    );
+};
+
+export const generateRandomEvent = (total_machine: number): IEvent => {
     const random = Math.random();
     if (random < 0.5) {
-        const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
-        return new MachineSaleEvent(saleQty, randomMachine());
+        const saleQty = Math.random() < 0.5 ? 10 : 20;
+        return new MachineSaleEvent(saleQty, randomMachine(total_machine));
     }
-    const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
-    return new MachineRefillEvent(refillQty, randomMachine());
+    const refillQty = Math.random() < 0.5 ? 3 : 5;
+    return new MachineRefillEvent(refillQty, randomMachine(total_machine));
 };
