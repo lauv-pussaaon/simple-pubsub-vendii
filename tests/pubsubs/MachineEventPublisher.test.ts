51416d367f3244607ecb51b8558ee64ff4490407
Lauv <pussaaon@gmail.com>
2024-03-24 11:00:06 +0700

    test publishing stock level warnings from events emitting

--- tests/pubsubs/MachineEventPublisher.test.ts
+++ tests/pubsubs/MachineEventPublisher.test.ts
@@ -1,7 +1,13 @@
 import { EventType } from "../../src/events/EventType";
 import { MachineRefillEvent } from "../../src/events/MachineRefillEvent";
 import { MachineSaleEvent } from "../../src/events/MachineSaleEvent";
+import { StockLevelInsufficientEvent } from "../../src/events/StockLevelInsufficientEvent";
+import { StockLevelLowEvent } from "../../src/events/StockLevelLowEvent";
+import { StockLevelOkEvent } from "../../src/events/StockLevelOkEvent";
 import { MachineEventPublisher } from "../../src/pubsubs/MachineEventPublisher";
+import { EventEmitter } from "events";
+import { StockWarningSubscriber } from "../../src/pubsubs/StockWarningSubscriber";
+import { mockMachineRepository } from "../../test-helpers/mockHelpers";
 
 describe("MachineEventPublisher Test Suite", () => {
     const mockMachineSaleSubscriber = {
@@ -83,6 +89,81 @@ describe("MachineEventPublisher Test Suite", () => {
 
             expect(mockMachineSaleSubscriber.handle).toHaveBeenCalledTimes(1);
             expect(mockMachineRefillSubscriber.handle).toHaveBeenCalledTimes(0);
+        });
+
+        it("should publish stock level warnings from emitted events", () => {
+            const machinesRepository = mockMachineRepository();
+            const machines = machinesRepository.getMachines().orElse([])!;
+            const machine1 = machines.at(0)!;
+            const stockWarningSubscriber = new StockWarningSubscriber(
+                mockMachineRepository()
+            );
+
+            const handleSpy = jest.spyOn(stockWarningSubscriber, "handle");
+            const handleStockLevelLowSpy = jest.spyOn(
+                stockWarningSubscriber,
+                "handleStockLevelLow"
+            );
+            const handleStockInsufficientSpy = jest.spyOn(
+                stockWarningSubscriber,
+                "handleStockInsufficient"
+            );
+            const handleStockLevelOkSpy = jest.spyOn(
+                stockWarningSubscriber,
+                "handleStockLevelOk"
+            );
+
+            const machineEventPublisher = new MachineEventPublisher();
+            machineEventPublisher.subscribe(
+                EventType.STOCK_LEVEL_LOW,
+                stockWarningSubscriber
+            );
+            machineEventPublisher.subscribe(
+                EventType.STOCK_LEVEL_INSUFFICIENT,
+                stockWarningSubscriber
+            );
+            machineEventPublisher.subscribe(
+                EventType.STOCK_LEVEL_OK,
+                stockWarningSubscriber
+            );
+
+            const eventEmitter = new EventEmitter();
+            eventEmitter.addListener(
+                EventType.STOCK_LEVEL_LOW,
+                (event: StockLevelLowEvent) => {
+                    machineEventPublisher.publish(event);
+                }
+            );
+            eventEmitter.addListener(
+                EventType.STOCK_LEVEL_INSUFFICIENT,
+                (event: StockLevelInsufficientEvent) => {
+                    machineEventPublisher.publish(event);
+                }
+            );
+            eventEmitter.addListener(
+                EventType.STOCK_LEVEL_OK,
+                (event: StockLevelOkEvent) => {
+                    machineEventPublisher.publish(event);
+                }
+            );
+
+            eventEmitter.emit(
+                EventType.STOCK_LEVEL_LOW,
+                new StockLevelLowEvent(machine1.id)
+            );
+            eventEmitter.emit(
+                EventType.STOCK_LEVEL_INSUFFICIENT,
+                new StockLevelInsufficientEvent(machine1.id)
+            );
+            eventEmitter.emit(
+                EventType.STOCK_LEVEL_OK,
+                new StockLevelOkEvent(machine1.id)
+            );
+
+            expect(handleSpy).toHaveBeenCalledTimes(3);
+            expect(handleStockLevelLowSpy).toHaveBeenCalledTimes(1);
+            expect(handleStockInsufficientSpy).toHaveBeenCalledTimes(1);
+            expect(handleStockLevelOkSpy).toHaveBeenCalledTimes(1);
         });
     });
 
